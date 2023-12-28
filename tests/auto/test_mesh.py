0a77a8e1cd14331d7cdc80c6f9c55d0deeaa565b
rleu <raphaelleu95@gmail.com>
2023-12-28 09:41:53 +0100

    pep8

--- tests/auto/test_mesh.py
+++ tests/auto/test_mesh.py
@@ -13,7 +13,6 @@ class MeshTest(unittest.TestCase):
         return mesh.read_mesh(path_to_coordinates=path_to_coordinates,
                               path_to_elements=path_to_elements)
 
-
     def test_read_mesh(self):
         z0, z1, z2, z3 = [0., 0.], [1., 0.], [1., 1.], [0., 1.]
         e0, e1 = [0, 1, 2], [0, 2, 3]
@@ -21,27 +20,27 @@ class MeshTest(unittest.TestCase):
         expected_elements = np.array([e0, e1])
 
         domain = MeshTest.get_test_mesh()
-        
+
         self.assertTrue(np.all(expected_coordinates == domain.coordinates))
         self.assertTrue(np.all(expected_elements == domain.elements))
 
     def test_provide_geometric_data(self):
         boundary_0 = np.array([[0, 1], [1, 2]], dtype=int)
         boundary_1 = np.array([[2, 3], [3, 0]], dtype=int)
-        
-        domain = MeshTest.get_test_mesh()
-        element2edges, edge2nodes, boundaries_to_edges = mesh.provide_geometric_data(
-            domain, boundary_0, boundary_1)
 
-        self.assertTrue( np.all(element2edges[0] == [0, 1, 2] ))
-        self.assertTrue( np.all(element2edges[1] == [2, 3, 4] ))
+        domain = MeshTest.get_test_mesh()
+        element2edges, edge2nodes, boundaries_to_edges = \
+            mesh.provide_geometric_data(domain, boundary_0, boundary_1)
+
+        self.assertTrue(np.all(element2edges[0] == [0, 1, 2]))
+        self.assertTrue(np.all(element2edges[1] == [2, 3, 4]))
         self.assertEqual(len(element2edges), 2)
 
-        self.assertTrue( np.all( edge2nodes[0] == [0, 1] ) )
-        self.assertTrue( np.all( edge2nodes[1] == [1, 2] ) )
-        self.assertTrue( np.all( edge2nodes[2] == [0, 2] ) )
-        self.assertTrue( np.all( edge2nodes[3] == [2, 3] ) )
-        self.assertTrue( np.all( edge2nodes[4] == [0, 3] ) )
+        self.assertTrue(np.all(edge2nodes[0] == [0, 1]))
+        self.assertTrue(np.all(edge2nodes[1] == [1, 2]))
+        self.assertTrue(np.all(edge2nodes[2] == [0, 2]))
+        self.assertTrue(np.all(edge2nodes[3] == [2, 3]))
+        self.assertTrue(np.all(edge2nodes[4] == [0, 3]))
         self.assertEqual(len(edge2nodes), 5)
 
         self.assertTrue(np.all(boundaries_to_edges[0] == [0, 1]))
