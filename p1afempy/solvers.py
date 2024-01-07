579a8342d8e70291624903cd8e7af31fb519044a
rleu <raphaelleu95@gmail.com>
2024-01-07 13:39:33 +0100

    implemented get_stiffness_matrix

--- p1afempy/solvers.py
+++ p1afempy/solvers.py
@@ -5,6 +5,37 @@ from scipy.sparse.linalg import spsolve
 from typing import Callable
 
 
+def get_stiffness_matrix(mesh: mesh.Mesh):
+    """
+    returns the stiffness matrix for the P1 FEM
+    with Legendre basis
+
+    parameters
+    ----------
+    mesh: mesh.Mesh
+        the mesh used for the construction of the
+        stiffness matrix
+
+    returns
+    -------
+    scipy.sparse.coo_matrix: the sparse stiffness matrix
+    """
+    c1 = mesh.coordinates[mesh.elements[:, 0], :]
+    d21 = mesh.coordinates[mesh.elements[:, 1], :] - c1
+    d31 = mesh.coordinates[mesh.elements[:, 2], :] - c1
+
+    # vector of element areas 4*|T|
+    area4 = 2 * (d21[:, 0]*d31[:, 1] - d21[:, 1] * d31[:, 0])
+
+    I = (mesh.elements[:, [0, 1, 2, 0, 1, 2, 0, 1, 2]].T).flatten(order='F')
+    J = (mesh.elements[:, [0, 0, 0, 1, 1, 1, 2, 2, 2]].T).flatten(order='F')
+    a = (np.sum(d21*d31, axis=1)/area4)
+    b = (np.sum(d31*d31, axis=1)/area4)
+    c = (np.sum(d21*d21, axis=1)/area4)
+    A = np.vstack([-2.*a+b+c, a-b, a-c, a-b, b, -a, a-c, -a, c])
+    return coo_matrix((A.flatten(order='F'), (I, J)))
+
+
 def solve_laplace(mesh: mesh.Mesh,
                   dirichlet: mesh.BoundaryCondition,
                   neumann: mesh.BoundaryCondition,
