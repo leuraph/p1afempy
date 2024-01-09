c65d924f612d928c35fc07d92cc377cdb06e2719
rleu <raphaelleu95@gmail.com>
2024-01-09 16:44:09 +0100

    failing test: refineRGB

--- p1afempy/tests/auto/test_mesh.py
+++ p1afempy/tests/auto/test_mesh.py
@@ -186,6 +186,48 @@ class MeshTest(unittest.TestCase):
         self.assertTrue(np.all(
             new_boundaries[2].boundary == refined_bc_2.boundary - 1))
 
+    def test_refineRGB(self) -> None:
+        # L-shaped Domain
+        path_to_coordinates = Path('tests/data/l_shape_coordinates.dat')
+        path_to_elements = Path('tests/data/l_shape_elements.dat')
+        path_to_bc_0 = Path('tests/data/l_shape_bc_0.dat')
+        path_to_bc_1 = Path('tests/data/l_shape_bc_1.dat')
+        path_to_bc_2 = Path('tests/data/l_shape_bc_2.dat')
+        domain = mesh.read_mesh(path_to_coordinates=path_to_coordinates,
+                                path_to_elements=path_to_elements)
+        marked_elements = np.array([0, 1, 3, 5])
+        refined_mesh, new_boundaries = \
+            mesh.refineRGB(mesh=domain,
+                           marked_elements=marked_elements,
+                           boundary_conditions=[
+                               mesh.read_boundary_condition(path_to_bc_0),
+                               mesh.read_boundary_condition(path_to_bc_1),
+                               mesh.read_boundary_condition(path_to_bc_2)])
+
+        path_to_refined_coordinates = Path(
+            'tests/data/refined_rgb/l_shape_coordinates_refined.dat')
+        path_to_refined_elements = Path(
+            'tests/data/refined_rgb/l_shape_elements_refined.dat')
+        expected_refined_mesh = mesh.read_mesh(
+            path_to_coordinates=path_to_refined_coordinates,
+            path_to_elements=path_to_refined_elements)
+        refined_bc_0 = mesh.read_boundary_condition(
+            Path('tests/data/refined_rgb/l_shape_boundary_0_refined.dat'))
+        refined_bc_1 = mesh.read_boundary_condition(
+            Path('tests/data/refined_rgb/l_shape_boundary_1_refined.dat'))
+        refined_bc_2 = mesh.read_boundary_condition(
+            Path('tests/data/refined_rgb/l_shape_boundary_2_refined.dat'))
+
+        self.assertTrue(np.all(
+            refined_mesh.coordinates == expected_refined_mesh.coordinates))
+        self.assertTrue(np.all(
+            refined_mesh.elements == expected_refined_mesh.elements - 1))
+        self.assertTrue(np.all(
+            new_boundaries[0].boundary == refined_bc_0.boundary - 1))
+        self.assertTrue(np.all(
+            new_boundaries[1].boundary == refined_bc_1.boundary - 1))
+        self.assertTrue(np.all(
+            new_boundaries[2].boundary == refined_bc_2.boundary - 1))
 
 
 if __name__ == '__main__':
