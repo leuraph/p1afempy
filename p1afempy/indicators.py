91cac781134937f0aaadf8ec29e9b2f56dcb35e6
rleu <raphaelleu95@gmail.com>
2024-01-11 11:37:18 +0100

    passing test: compute_eta_r

--- p1afempy/indicators.py
+++ p1afempy/indicators.py
@@ -1,15 +1,61 @@
 import numpy as np
-from p1afempy import mesh
+from p1afempy.mesh import Mesh, provide_geometric_data, BoundaryCondition
 from typing import Callable
 
 
-def compute_eta_r(x: np.ndarray, mesh: mesh.Mesh,
-                  dirichlet: mesh.BoundaryCondition,
-                  neumann: mesh.BoundaryCondition,
+def compute_eta_r(x: np.ndarray, mesh: Mesh,
+                  dirichlet: BoundaryCondition,
+                  neumann: BoundaryCondition,
                   f: Callable[[np.ndarray], float],
                   g: Callable[[np.ndarray], float]) -> np.ndarray:
     """
     # TODO doc
     """
-    # TODO implement
-    pass
+    boundary_conditions = [dirichlet, neumann]
+    element2edges, edge2nodes, boundaries_to_edges = \
+        provide_geometric_data(domain=mesh,
+                               boundaries=boundary_conditions)
+
+    # first vertex of elements and corresponding edge vectors
+    c1 = mesh.coordinates[mesh.elements[:, 0], :]
+    d21 = mesh.coordinates[mesh.elements[:, 1], :] - c1
+    d31 = mesh.coordinates[mesh.elements[:, 2], :] - c1
+
+    # vector of element volumes 2*|T|
+    area2 = d21[:, 0] * d31[:, 1] - d21[:, 1] * d31[:, 0]
+
+    # compute curl
+    tmp1 = x[mesh.elements[:, 1]] - x[mesh.elements[:, 0]]
+    tmp2 = x[mesh.elements[:, 2]] - x[mesh.elements[:, 0]]
+    u21 = np.column_stack([tmp1, tmp1])
+    u31 = np.column_stack([tmp2, tmp2])
+    curl = (d31 * u21 - d21 * u31) / np.column_stack([area2, area2])
+
+    # compute edge terms hE*(duh/dn) for uh
+    dudn21 = np.sum(d21 * curl, axis=1)
+    dudn13 = -np.sum(d31 * curl, axis=1)
+    dudn32 = -(dudn13 + dudn21)
+    etaR = np.bincount(
+        element2edges.flatten(order='F'),
+        np.hstack([dudn21, dudn32, dudn13]),
+        minlength=edge2nodes.shape[0])
+
+    # incorporate Neumann data
+    if neumann.boundary.size > 0:
+        cn1 = mesh.coordinates[neumann.boundary[:, 0], :]
+        cn2 = mesh.coordinates[neumann.boundary[:, 1], :]
+        gmE = g((cn1+cn2) / 2.)
+        neumann2edges = boundaries_to_edges[1]
+        etaR[neumann2edges] = etaR[neumann2edges] - np.sqrt(np.sum(
+            np.square(cn2-cn1),
+            axis=1)) * gmE
+
+    # incorporate Dirichlet data
+    dirichlet2edges = boundaries_to_edges[0]
+    etaR[dirichlet2edges] = 0
+    # assemble edge contributions of indicators
+    etaR = np.sum(np.square(etaR[element2edges]), axis=1)
+    # add volume residual to indicators
+    fsT = f((c1+(d21+d31) / 3.))
+    etaR = etaR + np.square(0.5 * area2 * fsT)
+    return etaR
