818359b72fac3cdb3b8658c554ee2af26180d953
rleu <raphaelleu95@gmail.com>
2024-01-15 16:57:58 +0100

    refactor: extracted example setup in seperate file

---
+++ p1afempy/tests/auto/example_setup.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+OMEGA = 7./4. * np.pi
+N_REFINEMENTS = 3
+
+
+def u(r: np.ndarray) -> float:
+    """analytical solution"""
+    return np.sin(OMEGA*2.*r[:, 0])*np.sin(OMEGA*r[:, 1])
+
+
+def f(r: np.ndarray) -> float:
+    """volume force corresponding to analytical solution"""
+    return 5. * OMEGA**2 * np.sin(OMEGA*2.*r[:, 0]) * np.sin(OMEGA*r[:, 1])
+
+
+def uD(r: np.ndarray) -> float:
+    """solution value on the Dirichlet boundary"""
+    return u(r)
+
+
+def g_right(r: np.ndarray) -> float:
+    return -2.*OMEGA*np.sin(OMEGA*r[:, 1])*np.cos(OMEGA*2.*r[:, 0])
+
+
+def g_upper(r: np.ndarray) -> float:
+    return OMEGA*np.sin(OMEGA*2.*r[:, 0]) * np.cos(OMEGA*r[:, 1])
+
+
+def g(r: np.ndarray) -> float:
+    out = np.zeros(r.shape[0])
+    right_indices = r[:, 0] == 1
+    upper_indices = r[:, 1] == 1
+    out[right_indices] = g_right(r[right_indices])
+    out[upper_indices] = g_upper(r[upper_indices])
+    return out
--- p1afempy/tests/auto/test_indicators.py
+++ p1afempy/tests/auto/test_indicators.py
@@ -2,38 +2,7 @@ import unittest
 from p1afempy import mesh, solvers, indicators
 from pathlib import Path
 import numpy as np
-
-
-def u(r: np.ndarray, omega: float = 7./4. * np.pi) -> float:
-    """analytical solution"""
-    return np.sin(omega*2.*r[:, 0])*np.sin(omega*r[:, 1])
-
-
-def f(r: np.ndarray, omega: float = 7./4. * np.pi) -> float:
-    """volume force corresponding to analytical solution"""
-    return 5. * omega**2 * np.sin(omega*2.*r[:, 0]) * np.sin(omega*r[:, 1])
-
-
-def uD(r: np.ndarray, omega: float = 7./4. * np.pi) -> float:
-    """solution value on the Dirichlet boundary"""
-    return u(r, omega=omega)
-
-
-def g_right(r: np.ndarray, omega: float = 7./4. * np.pi) -> float:
-    return -2.*omega*np.sin(omega*r[:, 1])*np.cos(omega*2.*r[:, 0])
-
-
-def g_upper(r: np.ndarray, omega: float = 7./4. * np.pi) -> float:
-    return omega*np.sin(omega*2.*r[:, 0]) * np.cos(omega*r[:, 1])
-
-
-def g(r: np.ndarray, omega: float = 7./4. * np.pi) -> float:
-    out = np.zeros(r.shape[0])
-    right_indices = r[:, 0] == 1
-    upper_indices = r[:, 1] == 1
-    out[right_indices] = g_right(r[right_indices], omega)
-    out[upper_indices] = g_upper(r[upper_indices], omega)
-    return out
+from example_setup import f, g, uD
 
 
 class IndicatorTest(unittest.TestCase):
