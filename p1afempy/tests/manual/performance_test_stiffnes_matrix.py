3144a7f1458163173835c72d006ae3a08ee27ed6
rleu <raphaelleu95@gmail.com>
2024-01-23 14:48:37 +0100

    stiffness matrix perf test: working python code

--- p1afempy/tests/manual/performance_test_stiffnes_matrix.py
+++ p1afempy/tests/manual/performance_test_stiffnes_matrix.py
@@ -1,4 +1,7 @@
+import time
+import copy
 import numpy as np
+import matplotlib.pyplot as plt
 from p1afempy import io_helpers
 from p1afempy import solvers
 from p1afempy import refinement
@@ -40,22 +43,79 @@ def main() -> None:
     boundary_1 = io_helpers.read_boundary_condition(path_to_boundary_1)
     boundaries = [boundary_0, boundary_1]
 
-    n_refinements = 5
+    # specifying statistics
+    n_refinements =11
+    n_repetetitions_each = 20
 
+    test_results: list[TestResult] = []
     for _ in range(n_refinements):
         n_elements = elements.shape[0]
 
-        # TODO measure
-        _ = solvers.get_stiffness_matrix(coordinates=coordinates,
-                                         elements=elements)
-        # TODO measure
+        test_result = copy.deepcopy(TestResult(n_elements=n_elements))
+        for _ in range(n_repetetitions_each):
+            start_time = time.process_time_ns()
+            _ = solvers.get_stiffness_matrix(coordinates=coordinates,
+                                             elements=elements)
+            end_time = time.process_time_ns()
+            test_result.add_time(time=(end_time - start_time)*1e-9)
 
+        test_results.append(copy.deepcopy(test_result))
         marked_elements = np.arange(n_elements)  # refine all elements
         coordinates, elements, boundaries = refinement.refineNVB(
             coordinates=coordinates,
             elements=elements,
             marked_elements=marked_elements,
             boundary_conditions=boundaries)
+
+    for result in test_results:
+        print(result.n_elements, result.get_statistics())
+
+    n_elements = np.array([result.n_elements for result in test_results])
+    means = np.array([np.mean(result.times) for result in test_results])
+    stdvs = np.array([np.std(result.times) for result in test_results])
+
+    # read matlab results
+    path_to_matlab_means = Path(
+        'tests/data/matlab_performance/'
+        'stiffness_matrix_assembly/means.dat')
+    path_to_matlab_stdevs = Path(
+        'tests/data/matlab_performance/'
+        'stiffness_matrix_assembly/stdevs.dat')
+    path_to_matlab_n_elements = Path(
+        'tests/data/matlab_performance/'
+        'stiffness_matrix_assembly/n_elements.dat')
+    matlab_means = np.loadtxt(path_to_matlab_means)
+    matlab_stdevs = np.loadtxt(path_to_matlab_stdevs)
+    matlab_n_elements = np.loadtxt(path_to_matlab_n_elements)
+
+    fig, ax = plt.subplots()
+    ax.set_title('P1AFEM(PY): Stiffnes Matrix Assembly Time')
+
+    # indices below this will be ignores because Matlab's
+    # cputime method simply returns zero for values 'too small'
+    cut_at = 4
+
+    # plot python results
+    ax.errorbar(n_elements[cut_at:], means[cut_at:], stdvs[cut_at:],
+                fmt='x', label='Python', color='green',
+                capsize=2, capthick=1, elinewidth=1, markersize=4)
+
+    # plot matlab results
+    ax.errorbar(matlab_n_elements[cut_at:],
+                matlab_means[cut_at:], matlab_stdevs[cut_at:],
+                fmt='s', label='Matlab', color='red',
+                capsize=2, capthick=1, elinewidth=1, markersize=4)
+
+    ax.set_xlabel('number of elements')
+    ax.set_ylabel('cpu time $/s$')
+
+    # Set the axis to log-log scale
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    ax.grid()
+    ax.legend(loc='best')
+    fig.savefig('stiffness_matrix_assembly.png')
 
 
 if __name__ == '__main__':
