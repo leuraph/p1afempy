7fb6433d777dc30eca88c4397d15a1337702df9d
rleu <raphaelleu95@gmail.com>
2024-01-25 09:07:24 +0100

    bugfix: updated imports in packages __init__.py

--- p1afempy/__init__.py
+++ p1afempy/__init__.py
@@ -3,5 +3,9 @@
 __author__="Raphael Leu"
 __version__="@VERSION@"
 
+from .data_structures import *
+from .indicators import *
+from .io_helpers import *
 from .mesh import *
+from .refinement import *
 from .solvers import *
