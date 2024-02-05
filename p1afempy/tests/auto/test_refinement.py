e6e8393f9842a8558aead0bc37b62fce27661221
rleu <raphaelleu95@gmail.com>
2024-02-05 10:48:31 +0100

    added: test_refinement.py with test for case no_boundary

---
+++ p1afempy/tests/auto/test_refinement.py
@@ -0,0 +1,69 @@
+import unittest
+import p1afempy.refinement as refinement
+import p1afempy.io_helpers as io_helpers
+from pathlib import Path
+import numpy as np
+
+
+class RefinementTest(unittest.TestCase):
+
+    def test_refineRG(self) -> None:
+        # ------------------------
+        # reading the initial mesh
+        # ------------------------
+        path_to_coordinates = Path(
+            'tests/data/trefined_rg/coordinates.dat')
+        path_to_elements = Path(
+            'tests/data/trefined_rg/elements.dat')
+        path_to_dirichlet = Path('tests/data/trefined_rg/dirichlet.dat')
+        path_to_neumann = Path('tests/data/trefined_rg/neumann.dat')
+        coordinates, elements = io_helpers.read_mesh(
+            path_to_coordinates=path_to_coordinates,
+            path_to_elements=path_to_elements)
+        dirichlet = io_helpers.read_boundary_condition(
+            path_to_boundary=path_to_dirichlet)
+        neumann = io_helpers.read_boundary_condition(
+            path_to_boundary=path_to_neumann)
+        boundaries = [dirichlet, neumann]
+
+        # ----------------
+        # case no_boundary
+        # ----------------
+        marked_elements = np.array([3])
+
+        new_coordinates, new_elements, new_boundaries = \
+            refinement.refineRG(
+                coordinates=coordinates,
+                elements=elements,
+                marked_elements=marked_elements,
+                boundaries=boundaries)
+
+        path_to_refined_coordinates = Path(
+            'tests/data/trefined_rg/case_no_boundary/new_coordinates.dat')
+        path_to_refined_elements = Path(
+            'tests/data/trefined_rg/case_no_boundary/new_elements.dat')
+        expected_coordinates, expected_elements = io_helpers.read_mesh(
+            path_to_coordinates=path_to_refined_coordinates,
+            path_to_elements=path_to_refined_elements)
+        expected_dirichlet = io_helpers.read_boundary_condition(
+            Path('tests/data/trefined_rg/case_no_boundary/new_dirichlet.dat'))
+        expected_neumann = io_helpers.read_boundary_condition(
+            Path('tests/data/trefined_rg/case_no_boundary/new_neumann.dat'))
+
+        self.assertTrue(np.all(
+            new_coordinates == expected_coordinates))
+        self.assertTrue(np.all(
+            new_elements == expected_elements - 1))
+        self.assertTrue(np.all(
+            new_boundaries[0] == expected_dirichlet - 1))
+        self.assertTrue(np.all(
+            new_boundaries[1] == expected_neumann - 1))
+
+        # TODO add case dirichlet
+        # TODO add case neumann
+
+        # TODO add case with many more elements exploting possible edge-cases
+
+
+if __name__ == '__main__':
+    unittest.main()
