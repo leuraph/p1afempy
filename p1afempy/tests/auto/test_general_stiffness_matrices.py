57c192c0d1578b063000267ed43e075da186ca8c
rleu <raphaelleu95@gmail.com>
2025-04-30 09:38:26 +0200

    chore: implemented `get_small_mesh()`

--- p1afempy/tests/auto/test_general_stiffness_matrices.py
+++ p1afempy/tests/auto/test_general_stiffness_matrices.py
@@ -1,10 +1,41 @@
+import numpy as np
 import unittest
+from p1afempy.data_structures import ElementsType, CoordinatesType
 
 
 class GeneralStiffnessMatrixTest(unittest.TestCase):
 
     def test_identity(self) -> None:
+        elements, coordinates = get_small_mesh()
+
+
+def get_small_mesh() -> tuple[ElementsType, CoordinatesType]:
+    """
+    returns a relatively coarse mesh
+
+    returns
+    -------
+    elements: ElementsType
+    coordinates: CoordinatesType
+    """
+    coordinates = np.array([
+        [0., 0.],
+        [1., 0.],
+        [1., 1.],
+        [0., 1.]
+    ])
+
+    elements = np.array([
+        [0, 1, 2],
+        [0, 2, 3]
+    ])
+
+    n_refinement_steps = 3
+    for _ in range(n_refinement_steps):
         pass
+        # TODO refine the mesh
+
+    return elements, coordinates
 
 
 if __name__ == '__main__':
